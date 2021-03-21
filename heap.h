2c5f0c77f6032529bd27c34e70109141e5e804b6
Zony Yu <zony249@gmail.com>
2021-03-20 18:09:21 -0600

    Heap now works

--- heap.h
+++ heap.h
@@ -34,7 +34,8 @@ public:
   // Put here by zony. This print function should be removed
   // before submission. Helps debug heap by printing all elements
   void print();
-
+  // remove this constructor as well when done;
+  BinaryHeap();
 private:
   // the array holding the heap
   std::vector< HeapItem<T, K> > heap;
@@ -60,6 +61,12 @@ void BinaryHeap<T, K>::print()
 	}
 }
 
+// Remove constructor when done
+template <class T, class K>
+BinaryHeap<T, K>:: BinaryHeap()
+{
+	this->heap = {};
+}
 
 
 template <class T, class K>
@@ -69,9 +76,69 @@ void BinaryHeap<T, K>::insert(const T& item, const K& key)
 	element.item = item;
 	element.key = key;
 	this->heap.push_back(element);
-	// NEED TO IMPLEMENT FIXHEAPUP
+	int idx = this->heap.size()-1;
+	this->fixHeapUp(idx);
+	cout << "insert size: " << this->heap.size() << endl; 
 }
 
+template <class T, class K>
+void BinaryHeap<T, K>::fixHeapUp(int i)
+{
+	if (i != 0)
+	{
+		int parent_idx = (i-1)/2;
+		if (this->heap[i].key < this->heap[parent_idx].key)
+		{
+			HeapItem<T, K> item = this->heap[i];
+			this->heap[i] = this->heap[parent_idx];
+			this->heap[parent_idx] = item;
+			this->fixHeapUp(parent_idx);
+		}
+	}
+}
 
+template <class T, class K>
+void BinaryHeap<T, K>::popMin()
+{
+	cout << "Pop min size: " << this->heap.size() << endl;
+	int idx = this->heap.size()-1;
+	if (this->heap.size() > 0)
+	{
+		this->heap[0] = this->heap[idx];
+		this->heap.pop_back();
+		this->fixHeapDown(0);
 
+	}
+}
+
+template <class T, class K>
+void BinaryHeap<T, K>::fixHeapDown(int i)
+{	
+	if (2*i+2 < this->heap.size())
+	{
+		HeapItem<T, K> lchild = this->heap[2*i+1];
+		HeapItem<T, K> rchild = this->heap[2*i+2];
+		int chosen;
+		if (lchild.key < rchild.key)
+		{
+			chosen = 2*i+1;
+		} else {
+			chosen = 2*i+2;
+		}
+		if (this->heap[chosen].key < this->heap[i].key)
+		{
+			HeapItem<T, K> temp = this->heap[i];
+			this->heap[i] = this->heap[chosen];
+			this->heap[chosen] = temp;
+			this->fixHeapDown(chosen);
+		}
+		
+	}
+}
+
+template <class T, class K>
+HeapItem<T, K> BinaryHeap<T, K>::min() const
+{	
+	return this->heap[0];
+}
 #endif
